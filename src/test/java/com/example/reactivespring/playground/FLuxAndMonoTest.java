17596b5df71909e785406794904da6fc45ced367
AltkomUser <test@altkom.pl>
2020-12-03 12:51:37 +0000

    Wiecej testow - onComplete, onError, map, filter

--- src/test/java/com/example/reactivespring/playground/FLuxAndMonoTest.java
+++ src/test/java/com/example/reactivespring/playground/FLuxAndMonoTest.java
@@ -2,7 +2,12 @@ package com.example.reactivespring.playground;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
+
+import java.time.Duration;
+import java.util.List;
 
 public class FLuxAndMonoTest {
 
@@ -42,4 +47,62 @@ public class FLuxAndMonoTest {
                 .verifyComplete();
 
     }
+
+    @Test
+    void givenFluXWith3Strings_should3StringsAndError(){
+        var exceptionMsg = "An error in the stream";
+        Flux<String> stringFlux = Flux.just("First", "Second", "Third")
+                .concatWith(Flux.error(new RuntimeException(exceptionMsg)))
+                .log();
+
+        StepVerifier
+                .create(stringFlux)
+                .expectNext("First")
+                .expectNextCount(2)
+//                .expectError(RuntimeException.class)
+                .expectErrorMessage(exceptionMsg)
+                .verify();
+
+    }
+
+    @Test
+    void monoComplete(){
+        StepVerifier.create(Mono.just("Just one Mono").log())
+                    .expectNext("Just one Mono")
+                .verifyComplete();
+    }
+
+    @Test
+    void monoError(){
+        StepVerifier.create(Mono.error(new RuntimeException()))
+                .expectError(RuntimeException.class)
+                .verify();
+    }
+
+    @Test
+    void givenPredicateShouldFilter(){
+        var iterable = List.of("Ania","Jan","Ola");
+        Flux<String> stringFlux = Flux.fromIterable(iterable)
+                .filter(s -> s.length()<=3)
+                .log();
+
+      StepVerifier.create(stringFlux)
+                    .expectNextCount(2)
+                    .verifyComplete();
+
+    }
+
+    @Test
+    void givenLambdaShouldTransofrm(){
+        var iterable = List.of("Ania","Jan","Ola");
+        Flux<String> stringFlux = Flux.fromIterable(iterable)
+                .map(String::toUpperCase)
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("ANIA","JAN","OLA")
+                .verifyComplete();
+
+    }
+
 }
